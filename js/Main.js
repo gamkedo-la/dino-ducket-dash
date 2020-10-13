4222faa10718af7c1fa0b4b98ede1111d9fa6c55
stebssbets <stebssbets@yahoo.com>
2020-10-13 15:57:25 +0700

    further tweaks to coin spawning (there was an instance where one coin was too far off the right edge). BUG: still can be an instance where a coin is not visible

--- js/Main.js
+++ js/Main.js
@@ -132,7 +132,7 @@ function spawnCoins(){
 
 		//WARM UP: limit ducket positioning so all of them are 100% on screen
 		var ducket = new ducketClass();
-		ducket.x = getRandomIntInclusive(DUCKET_WIDTH,canvas.width - DUCKET_WIDTH*3);
+		ducket.x = getRandomIntInclusive(DUCKET_WIDTH,canvas.width - DUCKET_WIDTH*3.25);
 		ducket.y = getRandomIntInclusive(DUCKET_HEIGHT,canvas.height - DUCKET_HEIGHT*5);
 		ducket.initCoin();
 		ducketList.push(ducket);
